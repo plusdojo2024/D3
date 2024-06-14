d2277c15a416827e5a6ac35240be2a71465f1fb3
plusdojo2024-matsunaga <matsunaga-satoru-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-14 16:10:07 +0900

    setting(暫定)

--- WebContent/js/setting.js
+++ WebContent/js/setting.js
@@ -1,1 +1,23 @@
- 
+ 'use strict';
+
+let formObj = document.getElementById("data_form");
+    let nameErrorObj = document.getElementById("name_error");
+    let heightErrorObj = document.getElementById("height_error");
+    let weightErrorObj = document.getElementById("weight_error");
+
+    formObj.onsubmit = function() {
+        if (!formObj.name.value) {
+            nameErrorObj.textContent = '名前を入力してください';
+            return false;
+        }
+
+        if (!formObj.height.value) {
+            heightErrorObj.textContent = '身長を入力してください';
+            return false;
+        }
+
+        if (!formObj.weight.value) {
+            weightErrorObj.textContent = '体重を入力してください';
+            return false;
+        }
+    }
