d5849f56bedf0c8a15a706a7848063d71026739b
plusdojo2024-kawai <kawai-moeka-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-17 14:59:32 +0900

    0617RecordDao更新

--- src/dao/RecordDao.java
+++ src/dao/RecordDao.java
@@ -63,4 +63,50 @@ public class RecordDao {
 		return result;
 	}
 
+
+	public boolean delete(int number) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/D3", "sa", "");
+
+			// SQL文を準備する
+			String sql = "DELETE FROM Record WHERE record_number=?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			pStmt.setInt(1, number);
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// 結果を返す
+		return result;
+	}
+
 }
