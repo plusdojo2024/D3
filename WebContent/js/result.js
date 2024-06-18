a405f8d40ea426a6ec49d504bcc6c0d688c1dd94
plusdojo2024-kawai <kawai-moeka-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 15:48:19 +0900

    regist.js更新

--- WebContent/js/result.js
+++ WebContent/js/result.js
@@ -1,14 +1,17 @@
 'use strict';
-         
-var day = new Date(document.lastModified);
-var y = day.getFullYear();
-var m = day.getMonth() + 1;
-var d = day.getDate();
-var week = new Array("日", "月", "火", "水", "木", "金", "土");
-var w = week[day.getDay()];
-if (m < 10) m = "0" + m;
-if (d < 10) d = "0" + d;
-date.innerHTML += y + "年" + m + "月" + d + "日 (" + w + ")" ;
+
+function makeDay(y, m, d){         
+	/*var day = new Date(document.lastModified);
+	var y = day.getFullYear();
+	var m = day.getMonth() + 1;
+	var d = day.getDate();*/
+	const date = new Date(y, m-1, d);
+	var week = new Array("日", "月", "火", "水", "木", "金", "土");
+	var w = week[date.getDay()];
+	if (m < 10) m = "0" + m;
+	if (d < 10) d = "0" + d;
+	document.getElementById("date").innerHTML += y + "年" + m + "月" + d + "日 (" + w + ")" ;
+}
 
 function deleteMessage(){
 	if(confirm("本当に削除しますか？")) {
@@ -17,4 +20,8 @@ function deleteMessage(){
 		alert('キャンセルしました');
 		event.preventDefault();
 	}
-}     
+}
+
+
+
+
