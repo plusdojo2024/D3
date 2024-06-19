440252368210090fce97510fb4eeea04180dcfcd
plusdojo2024-shiiku <shiiku-haruya-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-19 13:05:59 +0900

    ActiveRegist

--- WebContent/WEB-INF/jsp/active_regist.jsp
+++ WebContent/WEB-INF/jsp/active_regist.jsp
@@ -1,47 +1,169 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>ルート登録(仮)</title>
-    </head>
-    <style>
-        button {
-            text-align: center;
-        }
-    </style>
-    <body>
-        <h2>マップ入力(仮)</h2>
-        <div class="header">
-            【マップ入力】 | <a href="/D3/ActiveRegistServlet" class="move">【その他の入力】</a>
-        </div>
+<head>
+<meta charset="UTF-8">
+<title>その他の入力</title>
+ <link rel = "stylesheet" href="/D3/css/style.css">
+</head>
+<body>
+	<div class="wrapper">
+ <div class="sidebar">
+    <h2>Menu</h2>
+    <ul>
+      <li><a href="/D3/ResultServlet" class="js-header-nav-link">ホーム<i></i></a></li>
+      <li><a href="/D3/RouteRegistServlet"class="js-header-nav-link">入力<i></i></a></li>
+      <ul class="drop1" ontouchstart="1">
+        <li class="category" >グループ</li>
+        <li class="category1"><a href="/D3/MyScheduleServlet">個人</a></li>
+        <li class="category2"><a href="/D3/GroupManagerServlet">グループ管理</a></li>
 
-        <main>
-            <select name="">
-                <option value="0">---</option>
-                <option value="1">---</option>
-                <option value="2">---</option>
-                <option value="3">---</option>
-                <option value="4">---</option>
-                <option value="5">---</option>
-            </select>
-            距離 km
-            消費カロリー kcal
-            <br>
 
-            <input type="button" name="regist" value="登録">
-            <input type="button" name="reset" value="リセット"><br>
+        <li class="category3">
+          <div><a href="/D3/GroupScheduleServlet">グループ1</a></div>
+          <div><a href="/D3/GroupScheduleServlet">グループ2</a></div>
+          <div><a href="/D3/GroupScheduleServlet">グループ3</a></div>
+          <div><a href="/D3/GroupScheduleServlet">グループ4</a></div>
+          <div><a href="/D3/GroupScheduleServlet">グループ5</a></div>
+          <div><a href="#">グループ6</a></div>
+          <div><a href="#">グループ7</a></div>
+          <div><a href="#">グループ8</a></div>
+          <div><a href="#">グループ9</a></div>
+          <div><a href="#">グループ10</a></div>
+          <div><a href="#">グループ11</a></div>
+          <div><a href="#">グループ12</a></div>
 
-            <p>履歴</p>
-            経路
-            方法
-            距離 km
-            消費カロリー kcal
-        </main>
 
-        <script>
+        </li>
 
-        </script>
-    </body>
+       <!-- <li class="category7">グループ 5</li>
+        <li class="category8"><a href="#"></a>グループ 6</li>
+        <li class="category9">グループ 7</li>
+        <li class="category10"><a href="#"></a>グループ 8</li>-->
+      </ul>
+
+      <li class="setting"><a href="/D3/SettingServlet">設定</a></li>
+      <li><a href="/D3/LogoutServlet" onclick="if(!confirm('本当にログアウトしますか？'))return false">ログアウト</a></li>
+    </ul>
+  </div>
+		<div class="main_content">
+			<!--ヘッダー-->
+			<div class="header">
+				<a href="/D3/RouteRegistServlet" class="move">【マップ入力】</a> | 【その他の入力】
+			</div>
+			<div class="info"></div>
+			<!--確認メッセージ-->
+			<p>${ActiveResult.message}</p>
+			<!--運動の種類フォーム-->
+
+			<form action="./ActiveRegistServlet" method="POST" class="active">
+
+				<input type="checkbox" id="check1" value="1" name="check1"
+					onchange="func1()" class="scales" />
+										 <!-- 運動種類1 -->
+					 <select name="active_number1" id="active_number1" onchange="myfunc(this.value)">
+					<option value="0"></option>
+					<option value="${ a1.activeName }">ウォーキング</option>
+					<option value="">ランニング</option>
+					<option value="3">野球</option>
+					<option value="4">バスケットボール</option>
+					<option value="5">ドッジボール</option>
+					<option value="6">バレーボール</option>
+					<option value="7">サッカー</option>
+										<!-- 時間1 -->
+				</select> <select name="time1" id="time1" class="leave"value="" onchange="calc()"> <!-- 時間を選んだら処理 -->
+
+					<option value=""></option>
+					<option value="10">10分</option>
+					<option value="20">20分</option>
+					<option value="30">30分</option>
+					<option value="40">40分</option>
+					<option value="50">50分</option>
+					<option value="60">60分</option>
+					<option value="70">70分</option>
+					<option value="80">80分</option>
+					<option value="90">90分</option>
+					<option value="100">100分</option>
+					<option value="110">110分</option>
+					<option value="120">120分</option>
+									<!-- 歩数1 -->
+				</select> <input type="text" id = "step1" name="step1" placeholder="歩数"  value="" onKeyup="calc2()">
+
+							<input type="text" name="kcal1" placeholder="消費カロリー"> <br>
+
+							<input type="checkbox" id="check2" value="2" name="check2"
+					onchange="func1()" class="scales" />
+										 <!-- 運動種類2 -->
+					 <select name="active_number2" id="active_number2" onchange="myfunc2(this.value)">
+					<option value="0"></option>
+					<option value="1">ウォーキング</option>
+					<option value="2">ランニング</option>
+					<option value="3">野球</option>
+					<option value="4">バスケットボール</option>
+					<option value="5">ドッジボール</option>
+					<option value="6">バレーボール</option>
+					<option value="7">サッカー</option>
+										 <!-- 時間2 -->
+				</select> <select name="time2" id="time2" class="leave" value="" onchange="calc3()"><!-- 時間を選んだら処理2 -->
+					<option value=""></option>
+					<option value="10">10分</option>
+					<option value="20">20分</option>
+					<option value="30">30分</option>
+					<option value="40">40分</option>
+					<option value="50">50分</option>
+					<option value="60">60分</option>
+					<option value="70">70分</option>
+					<option value="80">80分</option>
+					<option value="90">90分</option>
+					<option value="100">100分</option>
+					<option value="110">110分</option>
+					<option value="120">120分</option>
+					 					<!-- 歩数2 -->
+				</select> <input type="text" id ="step2" name="step2" placeholder="歩数" value="" onKeyup="calc4()">
+				<input type="text" name="kcal2" placeholder="消費カロリー"> <br>
+
+        <input type="checkbox" id="check3" value="3" name="check3"
+        onchange="func1()" class="scales" />
+        	 							<!-- 運動種類3 -->
+         <select name="active_number3" id="active_number3" onchange="myfunc3(this.value)">
+					<option value="0"></option>
+					<option value="1">ウォーキング</option>
+					<option value="2">ランニング</option>
+					<option value="3">野球</option>
+					<option value="4">バスケットボール</option>
+					<option value="5">ドッジボール</option>
+					<option value="6">バレーボール</option>
+					<option value="7">サッカー</option>
+										 <!-- 時間3 -->
+         </select> <select name="time3" id="time3" class="leave" value="" onchange="calc5()">
+         <option value=""></option>
+					<option value="10">10分</option>
+					<option value="20">20分</option>
+					<option value="30">30分</option>
+					<option value="40">40分</option>
+					<option value="50">50分</option>
+					<option value="60">60分</option>
+					<option value="70">70分</option>
+					<option value="80">80分</option>
+					<option value="90">90分</option>
+					<option value="100">100分</option>
+					<option value="110">110分</option>
+					<option value="120">120分</option>
+        								 <!-- 歩数3 -->
+       </select> <input type="text" id ="step3" name="step3" placeholder="歩数" onKeyup="calc6()">
+      <input type="text" name="kcal3" placeholder="消費カロリー"> <br>
+
+								<input type="submit" name="submit" value="登録" class="button">
+
+
+
+			</form>
+		</div>
+		</div>
+
+		<script type="text/javascript" src="D3/active_regist.js">
+
+		</script>
+</body>
 </html>
--- src/servlet/ActiveRegistServlet.java
+++ src/servlet/ActiveRegistServlet.java
@@ -1,6 +1,9 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +11,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.RecordDao;
+import model.Active;
+import model.LoginUser;
+import model.Record;
+import model.ResultMessage;
+
 
 /**
  * Servlet implementation class ActiveRegistServlet
@@ -19,25 +30,199 @@ public class ActiveRegistServlet extends HttpServlet {
     /**
      * @see HttpServlet#HttpServlet()
      */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+
+
+		HttpSession session = request.getSession();
+//		LoginUser loginUser = (LoginUser)session.getAttribute("loginUser");
+//		if (loginUser == null) {
+//			response.sendRedirect("/D3/LoginServlet");
+//			return;
+//		}
+		//セッションスコープのデータを受け取る
+		LoginUser loginUser = (LoginUser)session.getAttribute("loginUser");
+		List<Active> acList = loginUser.getAcList();
+		//session.setAttribute("id", new LoginUser(id));
+
+		//運動名、メッツ値、運動番号、運動種類を受け取る部分
+		//一時的にコメントアウトしてあります。
+//		Active a1 = loginUser.getPickupAcList(1);
+//		request.setAttribute("a1", a1);
+//		Active a2 = loginUser.getPickupAcList(2);
+//		request.setAttribute("a2", a2);
+//		Active a3 = loginUser.getPickupAcList(3);
+//		request.setAttribute("a3", a3);
+//		Active a4 = loginUser.getPickupAcList(4);
+//		request.setAttribute("a4", a4);
+//		Active a5 = loginUser.getPickupAcList(5);
+//		request.setAttribute("a5", a5);
+//		Active a6 = loginUser.getPickupAcList(6);
+//		request.setAttribute("a6", a6);
+//		Active a7 = loginUser.getPickupAcList(7);
+//		request.setAttribute("a7", a7);
+//		Active a8 = loginUser.getPickupAcList(8);
+//		request.setAttribute("a8", a8);
+//		Active a9 = loginUser.getPickupAcList(9);
+//		request.setAttribute("a9", a9);
+//		Active a10 = loginUser.getPickupAcList(10);
+//		request.setAttribute("a10", a10);
+
+
+
+
+
+
+
+
+		Calendar nowDate = Calendar.getInstance();
+		String year = String.valueOf(nowDate.get(Calendar.YEAR));
+		String month = String.valueOf(nowDate.get(Calendar.MONTH)+1);
+		String day = String.valueOf(nowDate.get(Calendar.DAY_OF_MONTH));
+		request.setAttribute("year", year);
+		request.setAttribute("month", month);
+		request.setAttribute("day", day);
+
+
+		// その他入力ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/active_regist.jsp");
+		dispatcher.forward(request, response);
+	}
+
     public ActiveRegistServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/active_regist.jsp");
-    	dispatcher.forward(request, response);
-	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+
+
+
+
+
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		HttpSession session = request.getSession();
+		LoginUser loginUser = (LoginUser)session.getAttribute("loginUser");
+	//	LoginUser loginUser = ;//ログインが完成したら削除する部分
+		int number			= loginUser.getNumber();
+		boolean[] result = {false, false, false};
+		String check1      = request.getParameter("check1");
+		if ("1".equals(check1)) {
+			String activeNumber1      = request.getParameter("active_number1");
+			String time1      = request.getParameter("time1");
+			String step1      = request.getParameter("step1");
+			String kcal1      = request.getParameter("kcal1");
+			Active active  	= new Active(1,1,1.0, "サッカー" ); //セッションに入っているマスターデータを取得する
+			int kind 		 	= active.getKind();
+			double value 	 	= 0.0;
+			String unit = "";
+			if(time1.equals("")) {
+				value = Double.parseDouble(step1);
+				unit = "歩数";
+			}else {
+				value = Double.parseDouble(time1);
+				unit = "分";
+			}
+
+			Date registDate  	= new Date();
+
+
+			double kcal 		= Double.parseDouble(kcal1);
+			RecordDao bDao = new RecordDao();
+			result[0] = bDao.insert(new Record(0, kind, value , unit, registDate, number, kcal ));
+
+		}else {
+			result[0] = true;
+		}
+			//二つ目のチェックボックス
+		String check2      = request.getParameter("check2");
+		if ("2".equals(check2)) {
+			String activeNumber2      = request.getParameter("active_number2");
+			String time2      = request.getParameter("time2");
+			String step2      = request.getParameter("step2");
+			String kcal2      = request.getParameter("kcal2");
+			Active active  	= new Active(1,1,1.0, "サッカー" ); //セッションに入っているマスターデータを取得する
+			int kind 		 	= active.getKind();
+			double value 	 	= 0.0;
+			String unit = "";
+			if(time2.equals("")) {
+				value = Double.parseDouble(step2);
+				unit = "歩数";
+			}else {
+				value = Double.parseDouble(time2);
+				unit = "分";
+			}
+
+			Date registDate  	= new Date();
+			//LoginUser loginUser = (LoginUser)session.getAttribute("id");
+			//LoginUser loginUser = new LoginUser(1);//ログインが完成したら削除する部分
+			//int number			= loginUser.getId();
+
+			double kcal 		= Double.parseDouble(kcal2);
+			RecordDao bDao = new RecordDao();
+			result[0] = bDao.insert(new Record(0, kind, value , unit, registDate, number, kcal ));
+
+		}else {
+			result[0] = true;
+		}
+		//３つ目のチェックボックス
+	String check3      = request.getParameter("check3");
+	if ("3".equals(check3)) {
+		String activeNumber3      = request.getParameter("active_number3");
+		String time3      = request.getParameter("time3");
+		String step3      = request.getParameter("step3");
+		String kcal3      = request.getParameter("kcal3");
+//		Active active  	= new Active(1,1,1.0, "サッカー" ); //セッションに入っているマスターデータを取得する
+//		int kind 		 	= active.getKind();
+		double value 	 	= 0.0;
+		String unit = "";
+		if(time3.equals("")) {
+			value = Double.parseDouble(step3);
+			unit = "歩数";
+		}else {
+			value = Double.parseDouble(time3);
+			unit = "分";
+		}
+
+		Date registDate  	= new Date();
+		//LoginUser loginUser = (LoginUser)session.getAttribute("loginUser");
+		//LoginUser loginUser = new LoginUser(1);//ログインが完成したら削除する部分
+		//int number			= loginUser.getId();
+
+		double kcal 		= Double.parseDouble(kcal3);
+		RecordDao bDao = new RecordDao();
+		result[0] = bDao.insert(new Record(0, kind, value , unit, registDate, number, kcal ));
+
+	}else {
+		result[0] = true;
+	}
+/*		int kind 		 	= Integer.parseInt(request.getParameter("kind"));
+		double value 	 	= Double.parseDouble(request.getParameter("value"));
+		String unit 	 	= request.getParameter("unit");
+		Date registDate  	= new Date();
+		int number		 	=Integer.parseInt(request.getParameter("number"));
+		double kcal 		= Double.parseDouble(request.getParameter("value"));
+		String check2      = request.getParameter("check1");
+		String check3      = request.getParameter("check1");
+		String step2      = request.getParameter("step2");
+		String step3      = request.getParameter("step3");
+		*/
+
+		// 登録処理を行う
+		if (result[0] && result[1] && result[2]) {	// 登録成功
+			request.setAttribute("result",
+			new ResultMessage("レコードを登録しました"));	// ResultMessage,javaからメッセージを表示
+		}
+		else {												// 登録失敗
+			request.setAttribute("result",
+			new ResultMessage("レコードを登録できませんでした。")); // ResultMessage,javaからメッセージを表示
+		}
+		// その他入力ページに戻る
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/active_regist.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
