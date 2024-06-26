635eee00c6758d399131ada87ef1227ef3ef285b
plusdojo2024-matsunaga <matsunaga-satoru-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-26 11:26:34 +0900

    setting, route_registテスト修正

--- WebContent/js/setting.js
+++ WebContent/js/setting.js
@@ -1,22 +1,29 @@
  'use strict';
 
+/* document.getElementById("form").onsubmit = function() {
+	const nameObj = document.getElementById("name").word.value;
+	if (nameObj == null) {
+
+	}
+ }; */
+
 let formObj = document.getElementById("data_form");
     let nameErrorObj = document.getElementById("name_error");
     let heightErrorObj = document.getElementById("height_error");
     let weightErrorObj = document.getElementById("weight_error");
 
-    formObj.onsubmit = function() {
-        if (formObj.name.value == null) {
+formObj.onsubmit = function() {
+    if (formObj.name.value == null) {
             nameErrorObj.textContent = '名前を入力してください';
             return false;
         }
 
-        if (!formObj.height.value) {
+        if (formObj.height.value == null) {
             heightErrorObj.textContent = '身長を入力してください';
             return false;
         }
 
-        if (!formObj.weight.value) {
+        if (formObj.weight.value == null) {
             weightErrorObj.textContent = '体重を入力してください';
             return false;
         }
